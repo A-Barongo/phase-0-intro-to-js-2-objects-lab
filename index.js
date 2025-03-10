964d9b2037a1d6f0cb42f1a30bf4216b80bb8a07
Allan <allanbarongo5@gmail.com>
2025-03-10 16:28:08 +0300

    Completed intro to js Objects lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+let employee={}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    let updateEmployee={...employee}
+    updateEmployee[key]=value
+    return updateEmployee
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(obj,key,value){
+    obj[key]=value
+    return obj
+}
+function deleteFromEmployeeByKey(employee,key){
+    let newEmployee={...employee} 
+    delete newEmployee[key]
+    return newEmployee
+}
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key];
+    return employee
+}
